14f4c7b4dbeb7d88bbfa4a9507ffafe908bb0df5
edersonOl <ederson.diego.oliveira@gmail.com>
2022-11-09 19:42:21 -0300

    Add the Function validationColorHex

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,19 @@ function validationNameColor(nomeCor) {
 
 function validationColorHex(corHexa) {
   const padrao = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-  let erro = false
+  let erro = false;
+  let haveNumber = false;
+
   if (corHexa.trim().length < 6) return false;
 
   corHexa.toUpperCase().split('').map(char => {
     if (!padrao.includes(char)) {
       erro = true
     }
+    haveNumber = haveNumber ? true : !isNaN(Number(char))
   })
+
+  if (!haveNumber) erro = true
  
   return !erro
 }
